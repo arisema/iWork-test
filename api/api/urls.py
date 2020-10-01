470b54369024e72a0d32508a530c2afc5bb08e56
Arisema Mezgebe <arisema.mezgebe@gmail.com>
2020-10-01 12:44:59 +0300

    Update user login and signup routes with tests.

--- api/api/urls.py
+++ api/api/urls.py
@@ -16,15 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
-from rest_framework.authtoken import views
-from .views import UserViewSet
+from .views import UserCreateView, UserLoginView, ItemViewSet
 
-
-router = routers.DefaultRouter()
-router.register(r'users', UserViewSet)
-# router.register(r'user/create', )
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('api-token-auth/', views.obtain_auth_token)
+    path('sign-up', UserCreateView.as_view(), name='user-create'),
+    path('login', UserLoginView.as_view(), name='user-login'),
+
 ]
